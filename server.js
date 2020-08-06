32ddfb26a384a2daa023d016ee643707476c4cee
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-08-05 18:09:49 -0700

    Functional server and updated page styling.

--- server.js
+++ server.js
@@ -8,12 +8,27 @@ var PORT = process.env.PORT || 8000;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(express.static('Images'));
 
 app.get("/", function(req, res) {
 res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/portfolio", function (req, res) {
-    res.sendFile(__dirname, "portfolio.html");
+    res.sendFile(path.join(__dirname, "portfolio.html"));
 });
 
+app.get("/contact", function(req, res) {
+    res.sendFile(path.join(__dirname, "contact.html"));
+});
+
+app.get("/aboutMe", function(req, res) {
+    res.sendFile(path.join(__dirname, "aboutMe.html"));
+});
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
+  
